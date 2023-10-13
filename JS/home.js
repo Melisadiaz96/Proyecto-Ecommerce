0132b9e7c63104c31cf444245f64dd83c8c50a0c
Melisa Diaz Iñiguez <melisamarina.diaz.md@gmail.com>
2023-10-13 01:11:06 -0300

    Se creo una funcion para pintar las cards automaticamente

---
+++ JS/home.js
@@ -0,0 +1,40 @@
+let productosGuardados = JSON.parse(localStorage.getItem("productosGuardados"));
+
+const containerCardsHTML = document.querySelector(".container-cards");
+
+function pintarProducto(productosAPintar){
+
+    containerCardsHTML.innerHTML = "";
+
+    productosAPintar.forEach(function(product,index){
+        
+
+        containerCardsHTML.innerHTML += 
+        `
+        <div class="cards ">
+        <div class="card-header">
+            <img src="${product.imagen}" alt="${product.nombre}">
+        </div>
+        <div class="card-body">
+            <h2 class="product-name">${product.nombre}</h2>
+            <p class="product-description">${product.descripcion}</p>
+            <div class="precios">
+                <div class="fecha-ingreso">
+                    <p>Fecha de Ingreso</p>
+                    <p>${product.fechaIngresado}</p>
+                </div>
+                <p class="precio">${product.precio}</p>
+            </div>
+        </div>
+        <div class="card-footer">
+            <button type="" class="button-detalles">Detalles</button>
+            <button type="">Comprar</button>
+        </div>
+    </div>
+        `
+    })
+
+    
+};
+
+pintarProducto(productosGuardados);
